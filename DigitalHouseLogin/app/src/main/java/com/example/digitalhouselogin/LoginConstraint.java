d781a38fc0a03857ef6e0efc05c2e86a909f54eb
michael <michaelpinheiro00@gmail.com>
2019-06-03 11:53:08 -0300

    constraint fazendo

---
+++ DigitalHouseLogin/app/src/main/java/com/example/digitalhouselogin/LoginConstraint.java
@@ -0,0 +1,13 @@
+package com.example.digitalhouselogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_constraint);
+    }
+}
